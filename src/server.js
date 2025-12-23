f653c12ea850e9e22980e30a60accf78506c6606
Darshit Kakadiya <darshit.kakadiya@shunyavkash.com>
2025-12-23 12:29:10 +0530

    404 Route Handler

--- src/server.js
+++ src/server.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { PORT } from './configs/environment.config.js';
-import { appDb } from './configs/dbConnection.config.js';
+import { PORT } from "./configs/environment.config.js";
+import { appDb } from "./configs/dbConnection.config.js";
 
 const app = express();
 const Port = PORT || 1101;
+
+//404 Route Handler
+app.use((req, res) => {
+  res.status(404).send(`404 - Route Not Found ==> ${req.originalUrl}`);
+});
 
 //start the server
 app.listen(Port, () => {
