f0f718b01a13df68b9ca1d2063982cdf42125f2b
Darshit Kakadiya <darshit.kakadiya@shunyavkash.com>
2025-12-25 16:08:46 +0530

    fix cors issue

--- src/server.js
+++ src/server.js
@@ -7,7 +7,12 @@ import { cookieParser } from "./middleware/cookieParser.middleware.js";
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:4000",
+    credentials: true,
+  })
+);
 app.use(cookieParser);
 
 //routes define
