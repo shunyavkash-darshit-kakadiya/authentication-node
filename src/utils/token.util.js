4261b358d848f41f97ae5de4b82602c6bd90d40a
Darshit Kakadiya <darshit.kakadiya@shunyavkash.com>
2025-12-25 13:56:37 +0530

    remove /2fa/login/verify api

--- src/utils/token.util.js
+++ src/utils/token.util.js
@@ -5,10 +5,6 @@ export function generateToken(payload, secret, expiresIn = "3d") {
   return jwt.sign(payload, secret, { expiresIn });
 }
 
-export function generate2FAToken(payload, secret, expiresIn = "15d") {
-  return jwt.sign(payload, secret, { expiresIn });
-}
-
 export function verifyToken(token, secret) {
   try {
     return jwt.verify(token, secret);
