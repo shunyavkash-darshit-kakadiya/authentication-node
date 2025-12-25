c84c1323b0ec27002f1eb2bf491487fdcd1973e6
Darshit Kakadiya <darshit.kakadiya@shunyavkash.com>
2025-12-25 12:53:33 +0530

    generate2FAToken

--- src/utils/token.util.js
+++ src/utils/token.util.js
@@ -5,6 +5,10 @@ export function generateToken(payload, secret, expiresIn = "3d") {
   return jwt.sign(payload, secret, { expiresIn });
 }
 
+export function generate2FAToken(payload, secret, expiresIn = "15d") {
+  return jwt.sign(payload, secret, { expiresIn });
+}
+
 export function verifyToken(token, secret) {
   try {
     return jwt.verify(token, secret);
