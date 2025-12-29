35199360812a442257ea0308e749e4356b7ef7af
Darshit Kakadiya <darshit.kakadiya@shunyavkash.com>
2025-12-29 22:07:42 +0530

    cors update

--- src/server.js
+++ src/server.js
@@ -8,12 +8,31 @@ import appRouter from "./app/app.route.js";
 const app = express();
 app.use(express.json());
 
-const corsOptions = {
-  origin: ["http://localhost:4000", "http://192.168.29.95:4000"],
-  credentials: true,
-};
+// const corsOptions = {
+//   origin: ["http://localhost:4000", "http://192.168.29.95:4000"],
+//   credentials: true,
+// };
+// app.use(cors(corsOptions));
 
-app.use(cors(corsOptions));
+const allowedOrigins = [
+  "http://localhost:4000",
+  "http://192.168.1.3:4000",  
+  "https://eff5b936af65.ngrok-free.app",
+];
+
+app.use(
+  cors({
+    origin(origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, origin);
+      }
+      return callback(new Error("CORS blocked"));
+    },
+    credentials: true,
+  })
+);
+
 app.use(cookieParser);
 
 //routes define
