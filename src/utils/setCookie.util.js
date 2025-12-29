d6ca61e413d8fe2d1ecf11486262f14574b39a03
Darshit Kakadiya <darshit.kakadiya@shunyavkash.com>
2025-12-29 16:20:22 +0530

    Update setCookie.util.js

--- src/utils/setCookie.util.js
+++ src/utils/setCookie.util.js
@@ -1,16 +1,18 @@
-export function setCookie(res, name, value, options = {}) {
-  const defaultOptions = {
-    httpOnly: true,
-    sameSite: "none",
-    secure: true,
-  };
-  // const isProd = process.env.NODE_ENV === "production";
+import { NODE_ENV } from "../configs/environment.config.js";
 
+export function setCookie(res, name, value, options = {}) {
   // const defaultOptions = {
   //   httpOnly: true,
-  //   sameSite: isProd ? "none" : "lax",
-  //   secure: isProd,
+  //   sameSite: "none",
+  //   secure: true,
   // };
+  const isProd = NODE_ENV === "production";
+
+  const defaultOptions = {
+    httpOnly: true,
+    sameSite: isProd ? "none" : "lax",
+    secure: isProd,
+  };
 
   const cookieOptions = { ...defaultOptions, ...options };
 
