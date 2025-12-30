d68da9d26a637373aff7d811229c076faf69c51d
Darshit Kakadiya <darshit.kakadiya@shunyavkash.com>
2025-12-30 12:24:32 +0530

    Update cors.middleware.js

--- src/middleware/cors.middleware.js
+++ src/middleware/cors.middleware.js
@@ -1,16 +1,17 @@
 import cors from "cors";
 
-const allowedOrigins = [
-  "http://localhost:4000",
-  "https://470026d33835.ngrok-free.app",
-];
-
 const corsMiddleware = cors({
   origin(origin, callback) {
     if (!origin) return callback(null, true);
-    if (allowedOrigins.includes(origin)) {
+
+    if (
+      origin.startsWith("http://localhost") ||
+      origin.startsWith("http://192.168.") ||
+      origin.endsWith(".ngrok-free.app")
+    ) {
       return callback(null, origin);
     }
+
     return callback(new Error("CORS blocked"));
   },
   credentials: true,
