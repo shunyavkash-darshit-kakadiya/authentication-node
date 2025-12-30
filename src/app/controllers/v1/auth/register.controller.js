76a57a8fc30340c8296c65ad2cf9736e8771a292
Darshit Kakadiya <darshit.kakadiya@shunyavkash.com>
2025-12-30 11:54:19 +0530

    Password must be at least 8 characters long

--- src/app/controllers/v1/auth/register.controller.js
+++ src/app/controllers/v1/auth/register.controller.js
@@ -5,9 +5,18 @@ const registerController = async (req, res) => {
   try {
     const { fullName, email, password } = req.body;
 
+    if (password.length < 8) {
+      return res.status(400).json({
+        message: "Password must be at least 8 characters long",
+        success: false,
+      });
+    }
+
     const existingUser = await Auth.findOne({ email });
     if (existingUser) {
-      return res.status(400).json({ message: "User already exists" });
+      return res
+        .status(400)
+        .json({ message: "User already exists", success: false });
     }
 
     const newUser = new Auth({
@@ -18,7 +27,9 @@ const registerController = async (req, res) => {
 
     await newUser.save();
 
-    res.status(201).json({ message: "User registered successfully", success: true });
+    res
+      .status(201)
+      .json({ message: "User registered successfully", success: true });
   } catch (error) {
     console.error("Error in registerController:", error);
     res.status(500).json({ message: "Internal Server Error", success: false });
